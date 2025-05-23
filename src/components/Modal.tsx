00c08cdc1511cf244f5dd13776d65768d338e23e
Gerald Ndulue <gerryndulue@gmail.com>
2025-05-23 17:50:51 +0100

    Final commit

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,66 +1,78 @@
 import { MdKeyboardArrowDown } from "react-icons/md";
 import Button from './Button';
+import { useForm } from '@formspree/react';
+import Lottie from "lottie-react";
+import success from "../assets/success.json";
 
 type ModalProps = {
   action : (arg: boolean) => void
 }
 
 function Modal({action}:ModalProps) {
+   const [state, handleSubmit] = useForm("xrbqvdra");
   return (
     <div onClick={() => action(false)} className='w-full fixed inset-0 bg-black/70 flex items-center justify-center'>
-        <div  onClick={(e) => e.stopPropagation()}  className='w-2/5 h-[80%] bg-white rounded-md p-6 px-16 flex flex-col gap-5 items-center justify-center relative z-10'>
-            <div className='flex flex-col gap-1 text-center text-2xl'>
-              <h1 className='text-black font-semibold'>Become a Distributor </h1>
-              <p className='text-sm text-subtle/70'>Become a distribbutor and get your own product line.</p>
-            </div>
+              {state.succeeded ? (
+                        <div className=' lg:w-2/5 h-[40%] bg-white flex-col items-center justify-center gap-5 p-6 px-16'>
+                            <h1 className='text-center text-xl font-semibold'>Thanks for Reaching out</h1>
+                            <p className='text-center text-base'>Your message has been safely <br/> delivered to our inbox. We’ll get back to you within 24–48 hours.</p>
+                            <Lottie animationData={success}  style={{height:'70%', width:"100%"}} />
+                        </div>
+                    ): (
+                <form action="https://formspree.io/f/xrbqvdra" method="POST" onSubmit={handleSubmit} onClick={(e) => e.stopPropagation()}  className='lg:w-2/5 w-[90%] mx-auto h-[80%] bg-white rounded-md p-6 lg:px-16 flex flex-col gap-5 items-center justify-center relative z-10'>
+                    <div className='flex flex-col gap-1 text-center text-2xl'>
+                      <h1 className='text-black font-semibold'>Become a Distributor </h1>
+                      <p className='text-sm text-subtle/70'>Become a distribbutor and get your own product line.</p>
+                    </div>
 
-            <div className='w-full flex flex-col gap-1'>
-              <label className='text-subtle/70 text-sm'>Company name</label>
-              <input className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
-            </div>
+                    <div className='w-full flex flex-col gap-1'>
+                      <label className='text-subtle/70 text-sm'>Company name</label>
+                      <input name="Company Name" required className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
+                    </div>
 
-            <div className='w-full relative flex flex-col gap-1'>
-              <label className='text-subtle/70 text-sm'>What product would you like to distribute</label>
-              <select className="w-full p-3 pr-10 border text-sm border-gray-300 rounded-lg appearance-none 
-               focus:outline-none focus:border-blue-50 focus:ring-2 text-gray-400 focus:ring-blue-100
-               transition-all duration-200 bg-white hover:border-gray-400"
-                defaultValue=""
-                aria-label="Select an option"
-                >
-                <option value="" disabled hidden>Select an option</option>
-                <option value="1">Breakfast Products</option>
-                <option value="2">Baking Products</option>
-              </select>
-              <div className="pointer-events-none absolute top-9 right-0 h-max flex items-center justify-center px-3">
-                <MdKeyboardArrowDown className="w-7 h-7 text-gray-400" />
-              </div>
-            </div>
+                    <div className='w-full relative flex flex-col gap-1'>
+                      <label className='text-subtle/70 text-sm'>What product would you like to distribute</label>
+                      <select name="Product Line" required className="w-full p-3 pr-10 border text-sm border-gray-300 rounded-lg appearance-none 
+                      focus:outline-none focus:border-blue-50 focus:ring-2 text-gray-400 focus:ring-blue-100
+                      transition-all duration-200 bg-white hover:border-gray-400"
+                        defaultValue=""
+                        aria-label="Select an option"
+                        >
+                        <option value="" disabled hidden>Select an option</option>
+                        <option value="Breakfast Products">Breakfast Products</option>
+                        <option value="Baking Products">Baking Products</option>
+                      </select>
+                      <div className="pointer-events-none absolute top-9 right-0 h-max flex items-center justify-center px-3">
+                        <MdKeyboardArrowDown className="w-7 h-7 text-gray-400" />
+                      </div>
+                    </div>
 
-            <div className='w-full flex items-start gap-5'>
-              <div className='w-full flex flex-col gap-1'>
-                <label className='text-subtle/70 text-sm'>Contact Number</label>
-                <input className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
-              </div>
+                    <div className='w-full flex items-start gap-5'>
+                      <div className='w-full flex flex-col gap-1'>
+                        <label className='text-subtle/70 text-sm'>Contact Number</label>
+                        <input name="Contact Number" required className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
+                      </div>
 
-              <div className='w-full flex flex-col gap-1'>
-                <label className='text-subtle/70 text-sm'>Email</label>
-                <input className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
-              </div>
-            </div>  
-            <div className='w-full flex items-start gap-5'>
-              <div className='w-full flex flex-col gap-1'>
-                <label className='text-subtle/70 text-sm'>City</label>
-                <input className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
-              </div>
+                      <div className='w-full flex flex-col gap-1'>
+                        <label className='text-subtle/70 text-sm'>Email</label>
+                        <input name="email" required className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
+                      </div>
+                    </div>  
+                    <div className='w-full flex items-start gap-5'>
+                      <div className='w-full flex flex-col gap-1'>
+                        <label className='text-subtle/70 text-sm'>City</label>
+                        <input name="city" required className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
+                      </div>
 
-              <div className='w-full flex flex-col gap-1'>
-                <label className='text-subtle/70 text-sm'>State</label>
-                <input className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
-              </div>
-            </div>  
+                      <div className='w-full flex flex-col gap-1'>
+                        <label className='text-subtle/70 text-sm'>State</label>
+                        <input name="state" required className='appearance-none w-full p-3 border-[1px] rounded-lg border-subtle/35 ' />
+                      </div>
+                    </div>  
 
-            <Button text='Enlist' type='button' theme='red' width='100%' />
-        </div>
+                    <Button text= {state.submitting ? 'Sending Message' : "Enlist"} type='submit' theme='red' width='100%' />
+                </form>
+                    )}
     </div>
   )
 }
--- src/routes/CommunityPage.tsx
+++ src/routes/CommunityPage.tsx
@@ -3,6 +3,9 @@ import image from '../assets/svgs/shop_hero_image.svg';
 import { BsTelephone } from 'react-icons/bs';
 import { MdOutlineEmail } from 'react-icons/md';
 import { motion } from 'framer-motion';
+import { useForm, ValidationError } from '@formspree/react';
+import Lottie from "lottie-react";
+import success from '../assets/success.json';
 
 const inputStyle = {
     small: ' w-full xl:max-w-sm p-3 rounded bg-gray-100 focus:ring-1 ring-primary outline-none',
@@ -10,6 +13,7 @@ const inputStyle = {
 };
 
 function CommunityPage() {
+      const [state, handleSubmit] = useForm("xvganbgk");
     return (
         <>
             <div className='bg-milk h-48'></div>
@@ -38,7 +42,7 @@ function CommunityPage() {
                 animate={{ y: 0, opacity: 1 }}
                 exit={{ y: -200, opacity: 0 }}
                 transition={{ type: 'spring', stiffness: 60, damping: 20, duration: 0.5 }}
-                className='bg-white flex flex-col xl:flex-row justify-center gap-8 xl:gap-4 p-8 '>
+                className='bg-white flex flex-col xl:flex-row justify-center gap-8 xl:gap-4 lg:p-8  p-5'>
                 <div className='order-2 xl:order-1 flex flex-col mx-auto xl:mx-0 gap-4 bg-white max-w-lg drop-shadow-md rounded-2xl p-8 '>
                     <div className='flex items-center gap-3'>
                         <span className='bg-just-red p-2 rounded-full '>
@@ -64,23 +68,42 @@ function CommunityPage() {
                     </a>
                 </div>
                 <div className='order-1 xl:order-2 bg-white drop-shadow-md rounded-2xl p-4 md:p-8'>
-                    <form className='flex flex-col gap-3'>
+                    {state.succeeded ? (
+                        <div className='flex-col items-center justify-center gap-5'>
+                            <h1 className='text-center text-xl font-semibold'>Thanks for Reaching out</h1>
+                            <p className='text-center text-base'>Your message has been safely <br/> delivered to our inbox. We’ll get back to you within 24–48 hours.</p>
+                            <Lottie animationData={success}  style={{height:300, width:"100%"}} />
+                        </div>
+                    ): (
+                    <form action='https://formspree.io/f/xvganbgk' method='POST' onSubmit={handleSubmit} className='flex flex-col gap-3'>
                         <div className='flex flex-wrap xl:flex-nowrap items-center gap-4 xl:gap-2'>
                             <input className={inputStyle.small} placeholder='Your Name*' type='text' name='name' required />
+                            <ValidationError 
+                                prefix="Email" 
+                                field="name"
+                                errors={state.errors}
+                            />
                             <input className={inputStyle.small} placeholder='Your Email*' type='email' name='email' required />
                             <input className={inputStyle.small} placeholder='Your Phone*' type='tel' name='phone' required />
-                            <input className={`md:block xl:hidden ${inputStyle.small}`} placeholder='Your Purpose' type='text' name='purpose' required />
                         </div>
+
+
                         <input className={`hidden xl:block ${inputStyle.wide}`} placeholder='Your Purpose' type='text' name='purpose' required />
-                        <textarea className={`resize-none ${inputStyle.wide}`} rows={6} placeholder='Your Message' name='name' required />
+                        <textarea className={`resize-none ${inputStyle.wide}`} rows={6} placeholder='Your Message' name='message' required />
+
+
                         <div className='flex justify-end'>
                             <button
                                 type='submit'
+                                disabled={state.submitting}
                                 className='rounded-md bg-just-red text-white font-medium hover:bg-red-600 py-2 px-3 text-sm md:text-base md:py-3 md:w-1/4 hover:drop-shadow-md'>
-                                Send Message
+                                {state.submitting ? 'Sending Message' : "Send Message"}
                             </button>
                         </div>
                     </form>
+                    )}
+
+
                 </div>
             </motion.section>
         </>
